0ca20003a0a962c0c8e85fae865a9cceba4e58c9
helenheidemaa <98057078+helenheidemaa@users.noreply.github.com>
2023-03-30 21:52:22 +0300

    Project structure update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+    <h1>Weather App</h1> 
+     
+     <footer>
+      This app was coded by Helen Heidemaa and is{" "} 
+     <a href= "https://github.com/helenheidemaa/reactweatherapp" 
+     target="_blank">open-sourced on GidHub</a>
+    </footer>
     </div>
   );
 }
